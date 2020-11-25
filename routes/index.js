781e872f6976c39a5381975ff9afbc2c154ffe7a
jon apagueno <jonathan.apagueno@gmail.com>
2020-11-24 23:57:41 -0800

    adds google oauth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
-
-const indexCtrl = require('../controllers/index');
-
 const router = express.Router();
+const passport = require('passport');
 
-router.get('/', indexCtrl.index);
+
+const Trail = require('../models/trail');
+
+router.get('/auth/google', passport.authenticate(
+    'google', {
+        scope: ['profile', 'email']
+}));
+
+router.get('/oauth2callback', passport.authenticate(
+    'google', {
+        successRedirect: '/trails',
+        failureRedirect: '/'
+}));
+
+router.get('/logout', function(req, res) {
+    req.logOut();
+    res.redirect('/');
+});
+
+router.get('/', function(req, res) {
+    res.render('index');
+});
 
 module.exports = router;
