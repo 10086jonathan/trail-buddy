556ee69309adbab7929aca7bf5c65ab990c5f098
jon apagueno <jonathan.apagueno@gmail.com>
2020-11-30 19:57:40 -0800

    add show controller

--- routes/trails.js
+++ routes/trails.js
@@ -6,6 +6,7 @@ const isAuthenticated = require('../utils/authorization');
 
 router.get('/', isAuthenticated, trailsCntrl.index);
 router.get('/new', isAuthenticated, trailsCntrl.new);
+router.get('/:id', isAuthenticated, trailsCntrl.show);
 router.post('/', isAuthenticated, trailsCntrl.create);
 
 module.exports = router;
