2bdfa5825f083618fdb65e5cd5fccb44b9a7299c
jon apagueno <jonathan.apagueno@gmail.com>
2020-11-30 19:16:07 -0800

    adds the new and create functionality

--- controllers/trails.js
+++ controllers/trails.js
@@ -1,15 +1,33 @@
 const Trail = require('../models/trail');
 
 module.exports = {
-    index
+    index,
+    new: newTrail,
+    create
 };
 
 function index(req, res) {
     console.log(req.query.name);
     Trail.find({}).populate('createdBy').exec(function (err, trails) {
-        res.render('trails/index', {
-            trails,
-            user: req.user
-        });
+        res.render('trails/index', { trails, user: req.user });
     });
 };
+
+function newTrail(req, res) {
+    res.render('trails/new', { user: req.user });
+};
+
+function create(req, res) {
+    // const trail = new Trail(req.body);
+    // trail.save(function(err) {
+    //     if(err) return res.render('trails/new', { user: req.user });
+    //     console.log(trail);
+    //     res.redirect('/trails', { user: req.user });
+    // });
+    req.body.createdBy = req.user._id;
+    Trail.create(req.body, function(err, trail) {
+        console.log(err);
+        res.redirect('/trails');
+    });
+};
+
